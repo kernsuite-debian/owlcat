37f6f1ff7e985dd2b063d1e440e15f3580169d65
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2020-06-05 14:08:45 +0200

    New upstream version 1.6.2

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 
 import os
-from distutils.core import setup
+from setuptools import setup
+
+install_requires = [
+      'astropy',
+      'numpy',
+      'matplotlib',
+      'python-casacore',
+      'meqtrees_cattery',
+      'scipy',
+      'astlib',
+      'astro-kittens',
+      'six',
+      'future',
+]
 
 setup(name='owlcat',
-      version='1.4.2',
+      version='1.6.2',
       description='miscellaneous utility scripts for manipulating radio interferometry data',
       author='Oleg Smirnov',
       author_email='Oleg Smirnov <osmirnov@gmail.com>',
       url='https://github.com/ska-sa/owlcat',
       packages=['Owlcat'],
-      requires=['pyfits', 'numpy', 'matplotlib', 'pyrap', 'meqtrees_cattery'],
+      install_requires=install_requires,
       scripts=['Owlcat/bin/' + i for i in os.listdir('Owlcat/bin')],
       data_files=[('Owlcat/bin/', ['Owlcat/bin/commands.list'])],
-     )
+)
